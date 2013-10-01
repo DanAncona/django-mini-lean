5285fb7e592e528b991272766d0c3315d6a35ef6
Dan Ancona <da@vizbang.com>
2013-10-01 14:19:13 -0700

    move details into popup div. multi element refactor working

--- djangominilean/views.py
+++ djangominilean/views.py
@@ -11,8 +11,8 @@ from djangominilean.models import Experiment
 
 # Architecture note: as it is now, the experimental variants and generation of the random variant all
 # happen here in the view. Eventually this will probably move either to a view decorator or to a custom
-# template processor, but as this is primarily a demo of the technique, we're keeping it here now for
-# maximum clarity.
+# template processor, but as this is primarily a demo of the technique, we're keeping it all here now
+# for maximum clarity.
 
 CURRENT_EXPERIMENT = 'test1'
 
@@ -23,21 +23,26 @@ EXPERIMENTS = \
     {
         'test1':
         {
-            'titles':
+            'title':
             [
                 'Welcome to Django Mini Lean',
                 'Try Django Mini Lean',
                 'Django Mini Lean Is So Metal'
             ],
-            'descriptions': 
+            'description': 
             [
                 'Django Mini Lean is an open source example of how to easily do split testing.',
                 'Want an easy way to do split testing in the django web framework? Give this open source demo a whirl!'
             ],
-            'images': 
+            'img': 
             [
                 'cow1.png',
                 'cow2.png'
+            ],
+            'cowtext':
+            [
+                'Please, enjoy this cow, and click the button below to share this on Facebook.',
+                'Please click the button below to share this on Facebook.'
             ]
         }
     }
@@ -45,106 +50,92 @@ EXPERIMENTS = \
 # Build the variant data structure
 def build_variant(code):
     variant = {}
-    for element in code.split('.'):
-        print element
+    [exp_code, variant_str] = code.split('-')
+    variants = variant_str.split('.')
+    for element in variants:
+        [var_label, var_index] = element.split(':')
+        print var_label, var_index
+        variant[var_label] = EXPERIMENTS[CURRENT_EXPERIMENT][var_label][int(var_index)]
+    print variant
     return variant
 
 # This is the main page, with the code for creating the experimental variant, and a code to track that
 # that is dropped into a cookie.
 def home(request):
     # If there's a testing code in the link, get it, drop it into the session and redirect
-    # so no one shares the link with the query string appended. The variant code returned
-    # is a string that looks like 0.1.0, where each integer corresponds to a component of the
-    # test.
-    code = request.GET.get('code', None)
-    if code:
-        request.session['code'] = code
-        print 'home: code in GET, redirecting'
+    # so no one shares the link with the query string appended.
+    variant_code_with_exp = request.GET.get('code', None)
+    if variant_code_with_exp:
+        request.session['code'] = variant_code_with_exp
         return HttpResponseRedirect('/')
     # If the code isn't in the query string, try the session...
     else:
-        code = request.session.get('code', None)
+        variant_code_with_exp = request.session.get('code', None)
 
     # If there's no testing code in the link or in the session,
     # make up a new random code & save it in the session. This
     # should work generally for any given EXPERIMENTS dict.
-    # code should be like titles:0.descriptions:1.images:0
+    # The code format is element_label:element_index,
+    # i.e. titles:0.descriptions:1.images:0
     current_exp = EXPERIMENTS[CURRENT_EXPERIMENT]
-    if code is None:
+    if variant_code_with_exp is None:
         variant_list = []
-        # go through each array and pick an index at random
+        # Go through each array and pick an index at random.
         for element in current_exp:
             element_index = randrange(0, len(current_exp[element]))
             variant_list.append(element + ':' + str(element_index))
-        
-        variant = str.join('.', variant_list)
-        print variant
-        code = CURRENT_EXPERIMENT + '-' + variant
-        request.session['code'] = code
-    else:
-        [excode, variant] = code.split('-')
-        variants = variant.split('.')
-        itext = int(variants[0])
-        iimage = int(variants[1])
-        idesc = int(variants[2])
 
-    variant = build_variant(code)
-#     title = current_exp['titles'][itext]
-#     description = current_exp['descriptions'][idesc]
-#     img = current_exp['images'][iimage]
-    
-    exp = Experiment.objects.get(code=CURRENT_EXPERIMENT, variant=variant)
+        variant_code = str.join('.', variant_list)
+        variant_code_with_exp = CURRENT_EXPERIMENT + '-' + variant_code
+        request.session['code'] = variant_code_with_exp
+    else:
+        [exp_code, variant_code] = variant_code_with_exp.split('-')
+        
+    print variant_code, variant_code_with_exp
+    # Now we definitely have our variant code, so build out the dictionary to hand off to the template.
+    variant_details = build_variant(variant_code_with_exp)
+    print variant_details
+
+    # And increment the pageviews for this variant.
+    exp = Experiment.objects.get(code=CURRENT_EXPERIMENT, variant=variant_code)
     exp.pageviews += 1
     exp.save()
 
     return render_to_response('home.html',
-            {'title': title, 'description': description, 'img': img, 'code': code,
+            {'variant_details': variant_details, 'code': variant_code_with_exp,
              'FB_APPID': settings.FB_APPID, 'FB_SECRET': settings.FB_SECRET},
             context_instance=RequestContext(request))
 
-# 
+# If the the rows for the Experiment aren't found in the db, calculate all the combinations
+# of the current experiment matrix via Cartesian product. Python's itertools makes this easy.
 def loadexperiment(request):
-    status = None
+    status = ''
     current_exp = EXPERIMENTS[CURRENT_EXPERIMENT]
+
     # Don't create the experiment if it's already in the db.
     existing_experiments = Experiment.objects.filter(code=CURRENT_EXPERIMENT)
     if len(existing_experiments) > 0:
-        status = "found experiment ", code, "- not created."
+        status = "found experiment ", CURRENT_EXPERIMENT, "- not created."
         return HttpResponse(status)
         
     # If it's not found, create rows in Experiment for each possible variant.
-    # make an array list of all the options
-    # then combinations_with_replacement
-    all_variants = []
+    # Make a list of all the options, with each array in element:index form.
+    variant_matrix = []
     for element in current_exp:
         variant_list = current_exp[element]
-        for variant in variant_list:
-            all_variants.append('%s:%i' % (element, variant_list.index(variant)))
-            
-    print all_variants
+        for i, item in enumerate(variant_list):
+            variant_list[i] = '%s:%i' % (element, i)
+        variant_matrix.append(variant_list)
 
-    x = [ 0, 1 ]
-    y = [ 0, 1, 2 ]
-    z = [ 0, 1]
-    m = [ x, y, z]
-    for t in itertools.product(*m):
-        print t
-    print [item for sublist in m for item in sublist]
-    
-#     num_elements = len(exp)
-# 
-#     for element in exp:
-# 
-#     
-#     for i in range(0, numvariants):
-#         for j in range(0, numvariants):
-#         	for k in range(0, numvariants):
-# 	            variant = str.join('.', [str(i), str(j), str(k)])
-# 	            newexp = Experiment(code=code, variant=variant)
-# 	            newexp.save()
-    status = "experiment ", code, " loaded"
-    
-    # Then generate all the posts to FB
+    # The Cartesian product of the matrix gives us all possible variants.
+    for t in itertools.product(*variant_matrix):
+        variant = str.join('.', list(t))
+        newexp = Experiment(code=CURRENT_EXPERIMENT, variant=variant)
+        newexp.save()
+        status += 'variant %s saved<br/>' % (variant)
+        
+    status += '<br/><br/>experiment ' + CURRENT_EXPERIMENT + ' loaded successfully.'
+
     return HttpResponse(status)
 
 def fbshare(request, code):
